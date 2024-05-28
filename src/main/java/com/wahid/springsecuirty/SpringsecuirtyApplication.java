673244e73cb8dc52064c591d16990ae0f31e1a08
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-05-27 20:47:44 -0700

    project setup

---
+++ src/main/java/com/wahid/springsecuirty/SpringsecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.wahid.springsecuirty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuirtyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wahid/springsecuirty/SpringsecuirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wahid.springsecuirty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuirtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
