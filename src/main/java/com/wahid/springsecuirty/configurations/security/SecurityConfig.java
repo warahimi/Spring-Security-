aef6435e203cc0de35b04c43c47b43da0a3eca2d
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-05-28 13:08:47 -0700

    added encryption logic using Bcrypt alg

--- src/main/java/com/wahid/springsecuirty/configurations/security/SecurityConfig.java
+++ src/main/java/com/wahid/springsecuirty/configurations/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -65,15 +67,15 @@ public class SecurityConfig {
     {
         // creating userDetails objects
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}1234") //
+                .password(passwordEncoder().encode("1234"))
                 .roles("USER")
                 .build();
         UserDetails user2 = User.withUsername("user2")
-                .password("{noop}1234") //
+                .password(passwordEncoder().encode("1234"))
                 .roles("USER")
                 .build();
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}1234") //
+                .password(passwordEncoder().encode("1234"))
                 .roles("ADMIN")
                 .build();
         JdbcUserDetailsManager userDetailsManager=
@@ -84,5 +86,10 @@ public class SecurityConfig {
         return userDetailsManager;
         //return new InMemoryUserDetailsManager(user1,user2, admin);
         // this will users credentials and roles in user details object
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder(); // Encryption algorithm , we can change if we want
     }
 }
